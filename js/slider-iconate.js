43e7d01b2737e4439eaa079e21fd8894764013af
golovewkin <golovininfo@gmail.com>
2015-12-02 18:44:00 +0200

    done

--- js/slider-iconate.js
+++ js/slider-iconate.js
@@ -5,57 +5,58 @@
         //============wow init===========================================
         new WOW().init();
         //============wow init===========================================
-
-        var settings = $.extend( {
-            'location'         : 'top',
-            'background-color' : 'blue'
-        }, options);
-
         var self = this;
 
-        var sliderData = $('.slider-data');
+        var settings = $.extend( {
+            'delay'         : 7000,
+            'effectAnimate': 'easeInOutBack',
+            optionsIconate : {                 //effects from iconate.js
+                from: 'fa-circle-o',
+                to: 'fa-circle-o',
+                animation: 'tada'
+            }
+        }, options);
+
+        var sliderContent = this.find('.slider-content');// =====this
+        var sliderData = this.find('.slider-data');
         var countSlide = $('.slider-data > div').length;
+        var sliderDots = this.find('.slider-dots');
+
         var counter = 1;
-        var sliders = []; // здесь будут хранится блоки с контентом
+        var sliderDataArr = []; // здесь будут хранится блоки с контентом
 
-        var sliderDots = $('.slider-dots');
-        var targetI = '';
-        var sliderWrap = $('.slider-wrapper');
+        var targetI, timerId;
 
-        var optionsSlider = {
-            from: 'fa-circle-o',
-            to: 'fa-circle-o',
-            animation: 'tada'
+        //helper function
+        var changeContent = function () {
+
+            sliderContent.html(sliderDataArr[counter]);
+            if (counter >= countSlide) {
+                counter = 1;
+            } else {
+                counter++;
+            }
         };
 
-        var timerId = setInterval(function () {
-            $('#num-' + counter).trigger('click')
-        }, 7000);
+        //autosliding
+        var autosliding = function () {
 
-        // Создаете настройки по умолчанию, которые расширяют ваши опции
+            timerId = setInterval(function () {
+                $('#num-' + counter).trigger('click')
+            }, settings.delay);
+        };
+        autosliding();
 
         var sliding = (function () {
 
-            var changeContent = function () {
-
-                self.html(sliders[counter]);
-                if (counter >= countSlide) {
-                    counter = 1;
-                } else {
-                    counter++;
-                }
-            };
-
-
             for (var i = 1; i <= countSlide; i++) {
-                sliders[i] = sliderData.find('.slider' + i);
-                sliderDots.append('<i class="fa fa-circle-o" id=num-' + i + '></i>')
+                sliderDataArr[i] = sliderData.find('.slider' + i);
+                sliderDots.append('<i class="fa '+ settings.optionsIconate.from +'" id=num-' + i + '></i>')
             }
 
-            sliderWrap.css('background-image', 'url("img/slider/' + counter + '.jpg")');
-            self.html(sliders[counter]);
+            self.css('background-image', 'url("img/slider/' + counter + '.jpg")');
+            sliderContent.html(sliderDataArr[counter]);
             counter++;
-
             sliderDots.find('i').first().addClass('active');
 
             sliderDots.on('click', function (e) {
@@ -67,23 +68,20 @@
                 if (targetI !== '') {
 
                     var dotSlider = document.getElementById('num-' + targetI);
-                    iconate(dotSlider, optionsSlider);
+                    iconate(dotSlider, settings.optionsIconate);
 
                     sliderDots.find('i').filter('.active').removeClass('active');
                     $('#num-' + targetI).addClass('active');
 
-                    self.animate({right: '60%'}, 1000, 'easeInOutBack', function () {
+                    sliderContent.animate({right: '60%'}, 1000, settings.effectAnimate, function () {
 
                         changeContent(targetI);
-                        sliderWrap.css('background-image', 'url("img/slider/' + targetI + '.jpg")');
+                        self.css('background-image', 'url("img/slider/' + targetI + '.jpg")');
 
-                        $(this).removeClass('bounceInLeft').removeClass('animated')
-                            .animate({right: 0}, 1000, 'easeInOutBack').stop(false, true);
+                        $(this).animate({right: 0}, 1000, settings.effectAnimate).stop(false, true);
                     });
 
-                    timerId = setInterval(function () {
-                        $('#num-' + counter).trigger('click')
-                    }, 7000);
+                    autosliding()
                 }
             });
 
